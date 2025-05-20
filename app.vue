b4a99923594d65d50c8a1d6bb21858e774e5f30b
Teinishi <79356047+Teinishi@users.noreply.github.com>
2025-05-20 21:37:29 +0900

    PWA導入

--- app.vue
+++ app.vue
@@ -8,6 +8,7 @@ useHead({
 
 <template>
   <NuxtRouteAnnouncer />
+  <NuxtPwaManifest />
   <UApp>
     <NuxtLayout>
       <NuxtPage />
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,15 +2,71 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxt/ui'],
+  devServer: {
+    host: '0.0.0.0'
+  },
+  modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxt/ui', '@vite-pwa/nuxt'],
   css: ['~/assets/css/main.css'],
   app: {
+    head: {
+      htmlAttrs: {
+        lang: 'ja'
+      },
+      title: 'Room Light Control',
+      meta: [
+        {name: 'theme-color', content: '#00c950'}
+      ],
+      link: [
+        {rel: 'icon', href: `/favicon.ico`, sizes: "48x48"},
+        {rel: 'icon', href: `/icon.svg`, sizes: "any", type: "image/avg+xml"},
+        {rel: 'apple-touch-icon', href: `/apple-touch-icon-180x180.png`},
+      ],
+    },
     pageTransition: {
       name: 'page',
       mode: 'out-in'
     }
   },
-  devServer: {
-    host: '0.0.0.0'
+  pwa: {
+    registerType: "autoUpdate",
+    manifest: {
+      name: 'Room Light Control',
+      theme_color: "#00c950",
+      lang: "ja",
+      short_name: "Light",
+      start_url: "/",
+      display: "standalone",
+      background_color: "#ffffff",
+      icons: [
+        {
+          "src": "pwa-64x64.png",
+          "sizes": "64x64",
+          "type": "image/png"
+        },
+        {
+          "src": "pwa-192x192.png",
+          "sizes": "192x192",
+          "type": "image/png"
+        },
+        {
+          "src": "pwa-512x512.png",
+          "sizes": "512x512",
+          "type": "image/png"
+        },
+        {
+          "src": "maskable-icon-512x512.png",
+          "sizes": "512x512",
+          "type": "image/png",
+          "purpose": "maskable"
+        }
+      ],
+    },
+    workbox: {
+      navigateFallback: null
+    },
+    devOptions: {
+      enabled: true,
+      type: "module"
+    },
   },
 })
