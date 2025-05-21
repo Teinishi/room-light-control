423e82cf3cde81bd8877d468db56ac4ad6091475
Teinishi <79356047+Teinishi@users.noreply.github.com>
2025-05-21 20:18:48 +0900

    レイアウトを微調整

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,12 +3,12 @@ const { data: timezone } = await useFetch('/api/timezone');
 </script>
 
 <template>
-  <div class="w-full min-h-dvh pt-40 pb-24 flex flex-col items-center">
-    <div class="fixed z-9999 top-0 p-4 pb-0 w-full h-36 bg-(--ui-bg) flex justify-center items-center">
+  <div class="w-full min-h-dvh pt-42 pb-22 flex flex-col items-center">
+    <div class="fixed z-9999 top-0 p-4 w-full h-40 bg-(--ui-bg) flex justify-center items-center">
       <RichClock :timezone="timezone ?? undefined" />
     </div>
     <NuxtPage />
-    <div class="fixed z-9999 bottom-0 p-4 pt-0 w-full h-20 bg-(--ui-bg) flex justify-center items-center">
+    <div class="fixed z-9999 bottom-0 p-4 w-full h-20 bg-(--ui-bg) flex justify-center items-center">
       <NavigationBar />
     </div>
   </div>
