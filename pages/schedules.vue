cca8239e7241541821ddfea109393f1168d027a5
Teinishi <79356047+Teinishi@users.noreply.github.com>
2025-05-22 23:25:27 +0900

    スケジュール一覧のアニメーション

--- pages/schedules.vue
+++ pages/schedules.vue
@@ -66,25 +66,45 @@ async function updateSchedule(id: number, patch: {
 </script>
 
 <template>
-  <div class="w-80 pb-13 flex flex-col gap-4">
-    <ScheduleItem
-      v-for="schedule in schedules?.schedules"
-      :key="schedule.id"
-      v-model:enabled="schedule.enabled"
-      v-model:hour="schedule.hour"
-      v-model:minute="schedule.minute"
-      v-model:weekdays="schedule.weekdays"
-      v-model:command-type="schedule.commandType"
-      @update:enabled="v => updateSchedule(schedule.id, {enabled: v})"
-      @update:hour="v => updateSchedule(schedule.id, {hour: v})"
-      @update:minute="v => updateSchedule(schedule.id, {minute: v})"
-      @update:weekdays="v => updateSchedule(schedule.id, {weekdays: v})"
-      @update:command-type="v => updateSchedule(schedule.id, {commandType: v})"
-      @delete="deleteSchedule(schedule.id)"
-    />
-    <div v-show="schedules?.schedules.length == 0" class="w-full py-16 text-center text-gray-500">
-      設定がありません
-    </div>
+  <div class="w-80 pb-13 relative">
+    <TransitionGroup
+      enter-active-class="transition"
+      leave-active-class="transition absolute"
+      enter-from-class="opacity-0 translate-y-10"
+      leave-to-class="opacity-0 -translate-y-10"
+      move-class="transition-transform"
+    >
+      <ScheduleItem
+        v-for="schedule in schedules?.schedules"
+        :key="schedule.id"
+        v-model:enabled="schedule.enabled"
+        v-model:hour="schedule.hour"
+        v-model:minute="schedule.minute"
+        v-model:weekdays="schedule.weekdays"
+        v-model:command-type="schedule.commandType"
+        class="w-full mt-4 first:mt-0 duration-300"
+        @update:enabled="v => updateSchedule(schedule.id, {enabled: v})"
+        @update:hour="v => updateSchedule(schedule.id, {hour: v})"
+        @update:minute="v => updateSchedule(schedule.id, {minute: v})"
+        @update:weekdays="v => updateSchedule(schedule.id, {weekdays: v})"
+        @update:command-type="v => updateSchedule(schedule.id, {commandType: v})"
+        @delete="deleteSchedule(schedule.id)"
+      />
+    </TransitionGroup>
+    <Transition
+      enter-active-class="transition-opacity"
+      leave-active-class="transition-opacity absolute"
+      enter-from-class="opacity-0"
+      leave-to-class="opacity-0"
+    >
+      <div
+        v-if="schedules?.schedules.length == 0"
+        key="no-schedules"
+        class="w-full py-16 text-center text-gray-500 absolute top-0 duration-300"
+      >
+        設定がありません
+      </div>
+    </Transition>
     <TimePickerModal @change="addSchedule">
       <UButton
         class="fixed bottom-22 left-0 right-0 m-auto w-80 rounded-lg text-2xl justify-center"
